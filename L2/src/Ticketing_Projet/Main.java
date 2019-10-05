32f9b752094c5e134e56b8fb731695ce59c2d111
Tom <thomas_martin_28@outlook.fr>
2019-10-05 22:23:04 +0200

    Test

---
+++ L2/src/Ticketing_Projet/Main.java
@@ -0,0 +1,12 @@
+package Ticketing_Projet;
+
+import javax.swing.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Window_User w = new Window_User();
+		System.out.println("Hello World");
+	}
+
+}
